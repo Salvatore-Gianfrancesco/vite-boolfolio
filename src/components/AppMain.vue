d18bfbd063ee1701d3610a80c14884131548aac3
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2023-01-19 16:23:43 +0100

    added cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,8 @@ export default {
     name: "AppMain",
     data() {
         return {
-            apiUrl: 'http://localhost:8000/api/projects'
+            projects: null,
+            apiUrl: 'http://localhost:8000'
         }
     },
     methods: {
@@ -14,26 +15,40 @@ export default {
                 .get(url)
                 .then(response => {
                     console.log(response.data.results);
+                    this.projects = response.data.results;
                 })
                 .catch(error => {
                     console.error(error);
                 });
+        },
+        getImage(img) {
+            console.log(img);
+            if (img) {
+                return this.apiUrl + '/storage/' + img;
+            }
+            return 'https://via.placeholder.com/600x300.png?text=Cover+Image';
         }
     },
     mounted() {
-        this.getProjects(this.apiUrl);
+        this.getProjects(this.apiUrl + '/api/projects');
     }
 }
 </script>
 
 <template>
     <main>
-        <div class="container my-4">
-            <h1>Projects</h1>
+        <div class="container my-4" v-if="projects">
+            <h1 class="mb-3">Projects</h1>
 
             <div class="row row-cols-5">
-                <div class="col">
-
+                <div class="col" v-for="project in projects.data">
+                    <div class="card h-100">
+                        <img class="card-img-top" :src="getImage(project.cover_img)" :alt="project.name">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ project.name }}</h4>
+                            <p class="card-text">{{ project.body }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
