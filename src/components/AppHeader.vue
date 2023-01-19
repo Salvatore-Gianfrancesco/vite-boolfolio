f370cb7eb229e94a0167abf760d2156b0073a8a8
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2023-01-19 15:44:14 +0100

    added header, main and called projects api endpoint

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,34 @@ export default {
 </script>
 
 <template>
-    <h1>Test</h1>
+    <header>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="">Boolfolio</a>
+                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </header>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,45 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            apiUrl: 'http://localhost:8000/api/projects'
+        }
+    },
+    methods: {
+        getProjects(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data.results);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    },
+    mounted() {
+        this.getProjects(this.apiUrl);
+    }
+}
+</script>
+
+<template>
+    <main>
+        <div class="container my-4">
+            <h1>Projects</h1>
+
+            <div class="row row-cols-5">
+                <div class="col">
+
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
